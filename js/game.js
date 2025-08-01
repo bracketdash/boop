8c653e72415aafc5ec12f1523220d5b6201f8400
Michael Hatch <michaelhatch@me.com>
2025-08-01 07:19:00 -0700

    plan out game logic

--- js/game.js
+++ js/game.js
@@ -38,11 +38,16 @@ class BoopGame {
   }
 
   evaluate(state, player) {
-    // TODO
+    // TODO: points for number of kittens on board (1 each)
+    // TODO: points for number of cats available (2 each)
+    // TODO: points for number of cats on board (3 each)
+    // TODO: set number of points (10) for 3 cats in a row (i.e. winning)
   }
 
   generateMoves(state, player) {
-    // TODO
+    // TODO: if player has at least one each of big and little pieces, there should be 2 X (empty cells on board) moves
+    // TODO: if player has just little or just big pieces, the number of moves should be equal to the number of empty cells on the board
+    // TODO: if player has no pieces available, return []; (this should skip their turn)
   }
 
   getState() {
@@ -50,7 +55,9 @@ class BoopGame {
   }
 
   isGameOver(state) {
-    // TODO
+    // TODO: return true if there are three of the same "O" or "T" in a row (vertical, horizontal, or diagonal)
+    // TODO: return true if there are 8 "O" or 8 "T" total on the board
+    // TODO: otherwise, return false
   }
 
   pushState(state) {
